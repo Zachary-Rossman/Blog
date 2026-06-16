bcb20e1bc1579529c861f96e0f636644c742c944
Zachary-Rossman <zrossman@yahoo.com>
2026-06-15 21:42:37 -0400

    Added post route to app/api/posts/route.ts. This will be used later with CreatePostForm so that it creates a new post.

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -4,3 +4,14 @@ import { NextResponse } from "next/server";
 export async function GET() {
     return NextResponse.json(posts);
 }
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  console.log(body);
+
+  return NextResponse.json({
+    message: "Post received",
+    body,
+  });
+}
